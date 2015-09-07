ce8e59c7299894ece7b22838c4b4103b61537b95
stephen_hara <trevorsmirnoff@gmail.com>
2015-09-06 23:32:54 -0400

    IT LIVES!!!

--- public/app.js
+++ public/app.js
@@ -1,13 +1,15 @@
+'use strict';
+
 angular.module('randomGroupGenerator', [ 'ngRoute' ])
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+.config(['$routeProvider', '$locationProvider', 
+    function($routeProvider, $locationProvider){
     $routeProvider.when('/', {
         templateUrl: 'base/base.html',
         controller: 'BaseCtrl'
+    })
+    .otherwise({
+        redirectTo: '/'
     });
-    // .otherwise({
-    //     redirectTo: '/'
-    // });
 
-    $locationProvider.html5mode({ enabled: true, requireBase: false});
-
+    $locationProvider.html5Mode({ enabled: true, requireBase: false});
 }]);
--- web.js
+++ web.js
@@ -7,6 +7,7 @@ var methodOverride = require('method-override');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
